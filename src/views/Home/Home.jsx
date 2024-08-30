b85db43d7ed424abd460d18d50bacfd9286ff6a3
GregoryG8 <115062265+CRuizD@users.noreply.github.com>
2024-08-30 01:20:24 -0500

    docs: create configuration for project

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+      <main className="container-main">prueba</main>
+    </div>
+  );
+}
