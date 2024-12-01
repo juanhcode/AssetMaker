b12ebd2596e1a9345a73cfe8d173eff3fbe3c811
Juan Hoyos <juanhoyos1347@gmail.com>
2024-12-01 01:53:42 -0500

    Feat: example

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ const BASE_URL = "http://34.44.169.14:8082";
 export const ENDPOINTS = {
   USERS: `${BASE_URL}/users`,
   REGISTER: `${BASE_URL}/users`,
-  LOGIN: `${BASE_URL} /rest/auth/login`,
+  LOGIN: `${BASE_URL}/rest/auth/login`,
   PORTAFOLIO: `${BASE_URL}/portfolios/users/1`,
 };
