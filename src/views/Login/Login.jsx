6b71f3ad0036f5108c94a6e4a2de77fe58fca4fb
Juan Hoyos <juanhoyos1347@gmail.com>
2024-12-01 02:43:01 -0500

    changes

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 import { FaUser, FaEye, FaEyeSlash } from "react-icons/fa";
-import { ENDPOINTS } from "config";
 
 function Login() {
+  const URL = process.env.REACT_APP_URL;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -12,11 +12,12 @@ function Login() {
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
+    //print("hola");
     e.preventDefault();
     setError(null);
     try {
       //http://34.45.127.11:8082/rest/auth/login
-      const response = await fetch(ENDPOINTS.LOGIN, {
+      const response = await fetch("http://34.44.169.14:8082/rest/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -24,7 +25,9 @@ function Login() {
         },
         body: JSON.stringify({ email, password }),
       });
-
+      if (!response.ok) {
+        throw new Error("Error en la solicitud");
+      }
       console.log("Login", response.data);
 
       if (!response.ok) {
